9801eff25ae23d4e18d135d09a1cc49eb80d1d34
dev-hoale <leconghoa11a1@gmail.com>
2024-09-22 18:16:24 +0700

    update

--- src/components/common/BtnBanks/index.js
+++ src/components/common/BtnBanks/index.js
@@ -15,32 +15,39 @@ const BtnBanks = ({ payment }) => {
   }, []);
 
   return (
-    <div className="d-flex flex-column gap-2 align-items-center">
-      <span>QR Ngân hàng</span>
-      {/* Zalo Button */}
-      {(payment?.payments || []).map((item, index) => (
-        <Button
-          key={index}
-          variant="light"
-          onClick={() =>
-            onOpenPopup({
-              visible: "true",
-              src: item.qrcode,
-              alt: item.name || "qr bank",
-            })
-          }
-          className="mb-3"
-        >
-          <img
-            src={item.logo_bank}
-            alt="icon bank"
-            srcset=""
-            width={20}
-            height={20}
-            className="rounded-circle"
-          />
-        </Button>
-      ))}
+    <div>
+      <p className="text-center mb-2">QR Ngân hàng</p>
+      <div className="d-flex flex-column gap-3 align-items-center mb-2">
+        {/* Zalo Button */}
+        {(payment?.payments || []).map((item, index) => (
+          <Button
+            key={index}
+            variant="light"
+            onClick={() =>
+              onOpenPopup({
+                visible: "true",
+                src: item.qrcode,
+                alt: item.name || "qr bank",
+              })
+            }
+          >
+            <div className="d-flex gap-1 align-items-center">
+              <img
+                src={item.logo_bank}
+                alt="icon bank"
+                srcset=""
+                width={20}
+                height={20}
+                className="rounded-circle"
+              />
+              <div>
+                <div>{item.bank_name}</div>
+                <div>{item.bank_number}</div>
+              </div>
+            </div>
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
